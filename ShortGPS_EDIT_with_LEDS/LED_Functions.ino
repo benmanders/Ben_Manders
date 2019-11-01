c6403a25100a1b71e3c8612cba6a9d2d3f313c2d
benmanders <57005806+benmanders@users.noreply.github.com>
2019-11-01 09:19:56 +0000

    Added Magnetometer Code

--- ShortGPS_EDIT_with_LEDS/LED_Functions.ino
+++ ShortGPS_EDIT_with_LEDS/LED_Functions.ino
@@ -1,8 +1,8 @@
 void displayLED(int bearingDeg)
 {
-  int LedPos = map(bearingDeg, 0, 360, 0, 15);
-  leds[ledPos] = CHSV(130, 255, 192);
-  FastLED.show();
+  int ledPos = map(bearingDeg, 0, 360, 0, 15);
   fill_solid(leds, NUM_LEDS, CRGB :: Black);
+  FastLED.show();
+  leds[ledPos] = CHSV(130, 255, 192);
   FastLED.show();
 }
