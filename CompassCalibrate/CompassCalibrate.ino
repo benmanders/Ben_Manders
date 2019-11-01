84d6b2cc6185274f184012a2e93ad5218307bf06
mhamilt <matt.hamilton@ed.ac.uk>
2019-11-01 10:33:09 +0000

    add calibration sketch

---
+++ CompassCalibrate/CompassCalibrate.ino
@@ -0,0 +1,83 @@
+/*
+   calibrate Adafruit_LSM9DS1
+*/
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h>
+//------------------------------------------------------------------------------
+// i2c
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+
+#define LSM9DS1_SCK A5
+#define LSM9DS1_MISO 12
+#define LSM9DS1_MOSI A4
+#define LSM9DS1_XGCS 6
+#define LSM9DS1_MCS 5
+// You can also use software SPI
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
+// Or hardware SPI! In this case, only CS pins are passed in
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
+//------------------------------------------------------------------------------
+const bool calibrationMode = false;
+float offsetX = 0.034;
+float offsetY = 0.225;
+float offsetZ = 0.155;
+float printScaling = 1000.;
+//------------------------------------------------------------------------------
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
+
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  while (!Serial)
+  {
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
+  }
+
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  setupSensor();
+  if (calibrationMode)
+    calibrateMagnetometer(5000);
+}
+
+void loop()
+{
+  lsm.read();
+  sensors_event_t a, m, g, temp;
+  lsm.getEvent(&a, &m, &g, &temp);
+ 
+  Serial.print((m.magnetic.x - offsetX) * printScaling);  // Serial.print(" gauss");
+  Serial.print("\t");
+  Serial.print((m.magnetic.y  - offsetY) * printScaling);    // Serial.print(" gauss");
+  Serial.print("\t");
+  Serial.println((m.magnetic.z - offsetZ)* printScaling);    // Serial.println(" gauss");
+  delay(20);
+}
+//------------------------------------------------------------------------------
